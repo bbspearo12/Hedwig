8de775f092f72c2e7bc19540a1748469eb0c3598
Chandana Mutgi <cmutgi@CMUTGI-GW-MB.local>
2016-09-19 01:54:04 -0700

    updated angular

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -15,7 +15,8 @@
             'infinite-scroll',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar',
-            'angularUtils.directives.dirPagination'
+            'angularUtils.directives.dirPagination',
+            'ngSanitize'
         ])
         .run(run);
 
